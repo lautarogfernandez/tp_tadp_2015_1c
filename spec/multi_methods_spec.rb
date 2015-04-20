e2091176af6c61d37414a20539de6caf46da04d3
lautarogfernandez <lautarogfernandez@hotmail.com.ar>
2015-04-20 15:53:55 -0300

    Agrego test y comienzo a ver como es partial_def (no anda todavia)

--- spec/multi_methods_spec.rb
+++ spec/multi_methods_spec.rb
@@ -1,26 +1,49 @@
 require 'rspec'
 require_relative '../src/multi_methods'
+require_relative '../src/partial_block'
 
 describe 'Tests de MultiMethods' do
 
+  #me parece que hay que incluir el modulo en la clase CLASS
+  class A
+    include MultiMethods
+  end
+
   it 'Prueba del calculo de la distancia parcial' do
-    calculo=MultiMethods.new()
+    calculo=A.new()
     expect(calculo.distancia_parametro_parcial(3,Numeric)).to eq(2)
     expect(calculo.distancia_parametro_parcial(3.0,Numeric)).to eq(1)
   end
 
   it 'Prueba del calculo de la distancia total' do
+    calculo=A.new()
     tipos_1=[Numeric, Integer]
     tipos_2=[Integer, Integer]
     tipos_3=[Integer, Integer,Integer, Integer,Integer, Integer]
     parametros_1=[3.0, 3]
     parametros_2=[3, 3]
     parametros_3=[3,3,3,3,3,3]
-    calculo=MultiMethods.new()
     expect(calculo.distancia_parametro_total(parametros_1,tipos_1)).to eq(3)
     expect(calculo.distancia_parametro_total(parametros_2,tipos_1)).to eq(4)
     expect(calculo.distancia_parametro_total(parametros_2,tipos_2)).to eq(2)
     expect(calculo.distancia_parametro_total(parametros_3,tipos_3)).to eq(6)
+  end
+
+  it 'Prueba de definicion parcial de un metodo unico' do
+    A.partial_def :concat, [String, Integer] do |s1,n|
+        s1 * n
+    end
+    expect(A.new().concat("Hello", 2)=="HelloHello").to be(true)
+  end
+
+  it 'Prueba de definicion parcial de dos metodos con la misma firma' do
+    A.partial_def :concat, [String, Integer] do |s1,n|
+      s1 * n
+    end
+    partial_def :concat, [Object, Object] do |o1, o2|
+      "Objetos concatenados"
+    end
+    expect(A.new.concat(Object.new, 3)=="Objetos concatenados").to be(true)
   end
 
 end
--- src/multi_methods.rb
+++ src/multi_methods.rb
@@ -1,4 +1,6 @@
-class MultiMethods
+require_relative '../src/partial_block'
+
+module MultiMethods
 
   def suma(lista)#se puede agregar a la clase que corresponda en ruby
     sum = 0
@@ -12,6 +14,11 @@ class MultiMethods
 
   def distancia_parametro_total(lista_parametros,lista_tipos_parametro)#arreglar segun corresponda segun suma
     total =suma(lista_parametros.collect { |parametro| distancia_parametro_parcial(parametro,lista_tipos_parametro[lista_parametros.index(parametro)])*(lista_parametros.index(parametro)+1) })
+  end
+
+  def self.partial_def (nombre,lista_parametros,&bloque)
+    bloque_parcial = PartialBlock.new(lista_parametros,&bloque)
+    self.class.define_method(nombre, bloque_parcial)
   end
 
 end
